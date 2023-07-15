bc6a807b63d3f6600fdd49100507807c4eba3f78
Serena Shakya <serenashakya@Ser.local>
2023-07-15 09:01:03 +0545

    first commit

---
+++ RockPaperScissors.py
@@ -0,0 +1,52 @@
+import random
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+images = [rock,paper,scissors]
+user_choice = int(input("Enter 0 for Rock, 1 for Paper, and 2 for Scissors"))
+
+if user_choice >= 3 or user_choice < 0:
+    print("Invalid Number")
+else:
+    print(images[user_choice])
+
+    computer_choice = random.randint(0,2)
+    print("Computer chose: ")
+    print(images[computer_choice])
+    print(computer_choice)
+    if computer_choice == user_choice:
+        print("Draw! !")
+    elif computer_choice == 0 and user_choice == 2:
+        print ("You Lose")
+    elif computer_choice > user_choice:
+        print("You Lose")
+    else:
+        print("You Win! !")
+    
+    # elif computer_choice == 2 and user_choice == 0:
+    #     print ("You Win! !")
